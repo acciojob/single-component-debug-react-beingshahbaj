b13e365ed649750bf2e8c4bfb335abb2a34686f0
shahbaj khan <130038256+beingshahbaj@users.noreply.github.com>
2024-01-24 08:35:43 +0000

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = () => {
+  const [isBallVisible, setBallVisibility] = useState(false);
+  const [ballPosition, setBallPosition] = useState({ left: 0 });
+
+  const handleStartClick = () => {
+    setBallVisibility(true);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "ArrowRight" || event.keyCode === 39) {
+      setBallPosition((prevPosition) => ({ left: prevPosition.left + 5 }));
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []); 
+
   return (
-    <div id="main">
-      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    <div>
+      {!isBallVisible && (
+        <button className="start" onClick={handleStartClick}>
+          Start
+        </button>
+      )}
+      {isBallVisible && (
+        <div
+          className="ball"
+          style={{
+            position: "absolute",
+            left: ballPosition.left + "px",
+          }}
+        ></div>
+      )}
     </div>
   );
 };
